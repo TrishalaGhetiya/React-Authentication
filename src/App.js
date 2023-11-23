e7525fa483fbe88a9e120f842838038813fe646f
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-11-23 18:07:26 +0530

    stays login after page refresh

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        {isLoggedIn && <Route path='/' exact>
           <HomePage />
-        </Route>
-        <Route path='/auth'>
+        </Route>}
+        {!isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+        {isLoggedIn && <Route path='/profile'>
           <UserProfile />
+        </Route>}
+        <Route path='*'>
+          <Redirect to='/auth' />
         </Route>
       </Switch>
     </Layout>
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -13,6 +13,7 @@ const MainNavigation = () => {
 
   const logoutHandler = () => {
     authCtx.logout();
+    localStorage.removeItem('token');
     history.replace('/auth');
   }
 
