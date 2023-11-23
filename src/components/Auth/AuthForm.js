b192d9c823c29904ae8d47e9cc66cbcf0402cd02
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-11-23 17:05:49 +0530

    change password code

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -10,7 +10,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,64 +18,42 @@ const AuthForm = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-      const enteredEmail = emailInputRef.current.value;
-      const enteredPassword = passwordInputRef.current.value;
-      setIsLoading(true);
-      if (isLogin) {
-        fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaMjEyYa1-VQ3dRX6GfB9u_vd7uM8God4',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then(async (res) => {
-          setIsLoading(false);
-          if(res.ok) {
-            const data = await res.json();
-            authCtx.login(data.idToken);
-          } else {
-            const data_1 = await res.json();
-            let errorMessage = 'Authentication Failed';
-            if (data_1 && data_1.error && data_1.error.message) {
-              errorMessage = data_1.error.message;
-            }
-            alert(errorMessage);
-          }
-        })
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaMjEyYa1-VQ3dRX6GfB9u_vd7uM8God4";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBaMjEyYa1-VQ3dRX6GfB9u_vd7uM8God4";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
       } else {
-         fetch(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBaMjEyYa1-VQ3dRX6GfB9u_vd7uM8God4",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: enteredEmail,
-              password: enteredPassword,
-              returnSecureToken: true,
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        ).then((res) => {
-          setIsLoading(false);
-          if(res.ok) {
-
-          } else {
-            return res.json().then((data) => {
-              let errorMessage = 'Authentication Failed';
-              if(data && data.error && data.error.message) {
-                errorMessage = data.error.message;
-              }
-              alert(errorMessage);
-            })
-          }
+        return res.json().then((data) => {
+          let errorMessage = "Authentication Failed";
+        throw new Error(errorMessage);
         })
       }
+    }).then((data) => {
+      authCtx.login(data.idToken);
+    }).catch(err => {
+      alert(err.message);
+    })
   };
 
   return (
@@ -96,7 +74,9 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
           {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,39 @@
+import { useContext, useRef } from 'react';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const newPasswordInputRef = useRef();
+const submitHandler = (e) => {
+  e.preventDefault();
+  const enteredNewPassword = newPasswordInputRef.current.value;
+
+  fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBaMjEyYa1-VQ3dRX6GfB9u_vd7uM8God4', {
+    method: 'POST',
+    body: JSON.stringify({
+      idToken: authCtx.token,
+      password: enteredNewPassword,
+      returnSecureToken: true
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => {
+    return res.json().then(data => {
+      console.log(data);
+    }).catch(err => { 
+      throw new Error(err);
+    })
+  }).catch(err => {
+    alert(err);
+  })
+}
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
